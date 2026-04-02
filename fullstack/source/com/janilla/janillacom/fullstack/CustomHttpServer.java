05db99acbe3168b6c2dd1f8052b4f6deea2b27f8
Diego Schivo <diego.schivo@gmail.com>
2026-04-02 08:53:32 +0200

    startThread

--- fullstack/source/com/janilla/janillacom/fullstack/CustomHttpServer.java
+++ fullstack/source/com/janilla/janillacom/fullstack/CustomHttpServer.java
@@ -25,12 +25,17 @@ package com.janilla.janillacom.fullstack;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.nio.channels.SocketChannel;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -54,15 +59,33 @@ import com.janilla.java.JavaReflect;
 @Context("fullstack")
 public class CustomHttpServer extends HttpServer {
 
+	protected static final Pattern BOT_REGEX = Pattern.compile("aws|config|docker|env|info|node|php|sql|wp|yml");
+
 	protected final JanillaBackend backend;
 
 	protected final JanillaFrontend frontend;
+
+	protected final Set<String> blacklist = new HashSet<>();
 
 	public CustomHttpServer(SSLContext sslContext, SocketAddress endpoint, HttpHandler handler, JanillaBackend backend,
 			JanillaFrontend frontend) {
 		super(sslContext, endpoint, handler);
 		this.backend = backend;
 		this.frontend = frontend;
+	}
+
+	@Override
+	protected Thread startThread(SocketChannel channel) {
+		String a;
+		try {
+			a = ((InetSocketAddress) channel.getRemoteAddress()).getAddress().getHostAddress();
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+		if (blacklist.contains(a))
+			throw new RuntimeException("Blacklisted " + a);
+
+		return super.startThread(channel);
 	}
 
 	@Override
@@ -79,6 +102,11 @@ public class CustomHttpServer extends HttpServer {
 		var l = lines.getFirst();
 
 		IO.println(dt.truncatedTo(ChronoUnit.SECONDS) + " " + a + " " + l);
+
+		if (BOT_REGEX.matcher(l).find()) {
+			blacklist.add(((InetSocketAddress) a).getAddress().getHostAddress());
+			throw new RuntimeException("Blacklisted " + ((InetSocketAddress) a).getAddress().getHostAddress());
+		}
 	}
 
 	@Override
@@ -97,6 +125,11 @@ public class CustomHttpServer extends HttpServer {
 				.sorted(Comparator.comparing(x -> x.name())).map(x -> x.value()).collect(Collectors.joining(" "));
 
 		IO.println(dt.truncatedTo(ChronoUnit.SECONDS) + " " + a + " " + s);
+
+		if (BOT_REGEX.matcher(s).find()) {
+			blacklist.add(((InetSocketAddress) a).getAddress().getHostAddress());
+			throw new RuntimeException("Blacklisted " + ((InetSocketAddress) a).getAddress().getHostAddress());
+		}
 	}
 
 	@Override
